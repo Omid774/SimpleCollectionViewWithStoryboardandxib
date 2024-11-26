5eea2766eac57bac52e1ecbc9dfe74f74135065a
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-11-26 13:34:25 +0330

    Initial Commit

---
+++ SimpleCollectionViewWithStoryboard&xib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCollectionViewWithStoryboard&xib
+//
+//  Created by Omid Heydarzadeh on 11/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
